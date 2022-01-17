8b7563747c401b5516e39df08039682bca1e911c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-16 23:07:29 -0600

    Navbar config added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <>
+            <footer
+                className="text-center text-white"
+                style={{ backgroundColor: "#f1f1f1" }}
+                >
+                {/* Grid container */}
+                <div className="container pt-4">
+                    {/* Section: Social media */}
+                    <section className="mb-4">
+                    {/* Facebook */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-facebook-f" />
+                    </a>
+                    {/* Twitter */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-twitter" />
+                    </a>
+                    {/* Google */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-google" />
+                    </a>
+                    {/* Instagram */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-instagram" />
+                    </a>
+                    {/* Linkedin */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-linkedin" />
+                    </a>
+                    {/* Github */}
+                    <a
+                        className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <i className="fab fa-github" />
+                    </a>
+                    </section>
+                    {/* Section: Social media */}
+                </div>
+                {/* Grid container */}
+                {/* Copyright */}
+                <div
+                    className="text-center text-dark p-3"
+                    style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+                >
+                    © 2022 Copyright: 
+                    <a className="text-dark" href="http://juleacapulco.com/">
+                    Jule Acapulco - Creado por Kevin Luciano
+                    </a>
+                </div>
+                {/* Copyright */}
+            </footer>
+        </>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,65 +3,55 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">
                 <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg"
                 alt='Jule-Restaurant' width={30} height={24}
                 className="d-inline-block align-text-top"/>
                 Jule Restaurant
                 </a>
-                <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="/navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                >
-                <span className="navbar-toggler-icon" />
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="/">
-                        INICIO
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        CONCEPTO
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        BUFFET
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        EVENTOS
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        GALERÍA
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        RESERVACIONES
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">
-                        CONTACTO
-                    </a>
-                    </li>
-                </ul>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Inicio
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Concepto
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Buffet
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Eventos
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Galería
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Reservaciones
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Contacto
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
             </nav>
         </>
     )
