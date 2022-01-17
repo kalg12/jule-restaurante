63e42f6185fe1b0af9d8fab0e2f0fc6ede7e5e71
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-16 23:47:05 -0600

    Navbar hamburguer icon updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">
-                <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg"
-                alt='Jule-Restaurant' width={30} height={24}
+                <img src="https://raw.githubusercontent.com/kalg12/jule-restaurante/main/src/img/flame.png"
+                alt='Jule-Restaurant' width={30} height={30}
                 className="d-inline-block align-text-top"/>
                 Jule Restaurant
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link" href="/">
                                 Inicio
