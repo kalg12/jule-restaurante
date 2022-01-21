8d6a08451176c32965cf01f38a91dcad945b8ade
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-21 01:21:37 -0600

    Concepto component added

--- src/components/Concepto.jsx
+++ src/components/Concepto.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 export const Concepto = () => {
     return (
-        <>
-            <h1>Concepto</h1>
-
-        </>
+        <div className='cover-concepto'>
+            <div className='cover-text-concepto'>
+                <h1>-Concepto-</h1>
+            </div>
+            <div className='cover-text-concepto'>
+                <p>En JULE FAST GRILL deléitate con el más suculento Buffet de Espadas Brasileñas y Cortes de Carne en Acapulco Guerrero.</p>
+                <p>Somos un restaurante familiar que integra lo mejor de Comida Fusión Brasileña y Mexicana en Acapulco para que disfrutes de exquisitos Desayunos, Comidas y Cenas.</p>
+                <p>Todos nuestros platillos son elaborados mediante procesos únicos que garantizan su abundante sabor, lo que nos ha permitido resaltar entre los mejores restaurantes con buffet en Acapulco.</p>
+                <p>Además, contamos con instalaciones de primera que incluyen servicio de bar con pantalla gigante para la transmisión de eventos deportivos; así como un servicio de excelencia para que goces de la mejor atención.</p>
+            </div>
+        </div>
     )
 }
