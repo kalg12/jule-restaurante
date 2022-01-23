23b0ecdfa8fc7f80a209b1a50076267866038783
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-23 10:06:40 -0600

    Urls fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Reservaciones } from './components/Reservaciones';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Navbar />
       <Switch>
         <Route path='/contacto'> <Contacto /> </Route>
