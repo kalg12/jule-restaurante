e7fb1929f3fe24e123226ce7e7f676efb606d1e6
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-23 10:41:18 -0600

    Home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Reservaciones } from './components/Reservaciones';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/jule-restaurante">
       <Navbar />
       <Switch>
         <Route path='/contacto'> <Contacto /> </Route>
