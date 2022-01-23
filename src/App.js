6bd4d0827932519a5e47bb1fd10a9d5254b4cf82
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-23 00:40:52 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Reservaciones } from './components/Reservaciones';
 
 function App() {
   return (
-    <Router basename='/React'>
+    <Router>
       <Navbar />
       <Switch>
         <Route path='/contacto'> <Contacto /> </Route>
