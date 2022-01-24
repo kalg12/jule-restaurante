eade1a61f573e95b754ac2e06cb6e34cfef25b83
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-23 23:46:25 -0600

    basename three

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Reservaciones } from './components/Reservaciones';
 
 function App() {
   return (
-    <Router basename='/jule-restaurant'>
+    <Router basename='/'>
       <Navbar />
       <Switch>
         <Route path='/contacto'> <Contacto /> </Route>
